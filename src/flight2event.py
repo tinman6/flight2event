2c2e36252e82cefabf2c1129b42c44a1ec28ff64
tinman6 <tinman6@users.noreply.github.com>
2019-12-29 10:12:32 -0500

    get airports from flightstats since flightaware uses icao instead of iata codes

--- src/flight2event.py
+++ src/flight2event.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import datetime
+
+from aadict import aadict
+import requests
 from suds.client import Client  # suds-jurko
 
 def getSchedule(airline, flightno, dt):
@@ -28,3 +31,28 @@ def getSchedule(airline, flightno, dt):
   }
 
 
+def getAirports(airline, flightno):
+  # flightaware uses icao codes instead of iata. using flightstats to
+  # supplement data
+  flight_url = 'https://www.flightstats.com/v2/api-next/flight-tracker/%s/%s/%s/%s/%s'
+  # todo: flightstats only have flight data for the week or so. using an
+  #       arbitrary date. this is not perfect since not all flights are daily,
+  #       and some flights are seasonal
+  d = datetime.date.today()
+  r= requests.get(url=(flight_url %
+                       (airline, flightno, d.year, d.month, d.day)))
+  a = aadict.d2ar(r.json()['data'])
+  return {
+    'origin': {
+      'name': a.departureAirport.name,
+      'iata': a.departureAirport.iata,
+      'terminal': a.departureAirport.terminal,
+      'timezone': a.departureAirport.timeZoneRegionName
+    },
+    'destination': {
+      'name': a.arrivalAirport.name,
+      'iata': a.arrivalAirport.iata,
+      'terminal': a.arrivalAirport.terminal,
+      'timezone': a.arrivalAirport.timeZoneRegionName
+    }
+  }
